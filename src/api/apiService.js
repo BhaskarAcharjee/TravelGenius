e9e9f93e706a0142da66cf5d2e32825b8d771518
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2024-06-23 18:42:23 +0530

    fixed eslint import export issue

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -39,31 +39,11 @@ export const getWeatherData = async (lat, lon) => {
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPENWEATHERMAP_KEY}`
       );
 
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// AI Recommendation API
-export const getAIRecommendation = async (city, days, budget, mood) => {
-  try {
-    const openai = new OpenAI({
-      apiKey: "sk-proj-aizwnbwo026nifX7LjAwT3BlbkFJiXIfu8TSgN9j0WOEWMaA", dangerouslyAllowBrowser: true 
-    });
-
-    const prompt = `Plan a ${days}-day trip to ${city} with a ${budget} budget. Focus on ${mood} activities.`;
-
-    const response = await openai.completions.create({
-      model: "gpt-3.5-turbo-instruct",
-      prompt: prompt,
-      max_tokens: 200,
-      temperature: 0.7,
-    });
-
-    return response.choices[0].text.trim();
-  } catch (error) {
-    console.error("Error fetching AI recommendation:", error);
-    return "Failed to get AI recommendation. Please try again.";
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("Development environment detected"); // for limiting api requests
   }
 };
